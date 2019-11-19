233ace981ef6faa86b94fe915447ae4b7f2ea0a6
Yuriy Koziy <yukoziy@gmail.com>
2019-11-19 18:27:48 -0500

    Route '/api/convert' is fully functional and passes functional tests

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
@@ -20,11 +20,19 @@ module.exports = function (app) {
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
+      if (initNum == "Invalid Number") {
+        if (initUnit== "Invalid Unit") {
+          res.status(400).json({'error':'invalid number and unit'});
+        }
+        res.status(400).json({'error':'invalid number'});
+      }
+      if (initUnit == "Invalid Unit") {
+        res.status(400).json({'error':'invalid unit'});
+      }
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+      res.json({initNum, initUnit, returnNum, returnUnit, 'string':toString});
     });
-    
+
 };
