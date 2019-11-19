7bde849f33c2a5e0b2443e31a1d21a9cf58c9147
Yuriy Koziy <yukoziy@gmail.com>
2019-11-18 20:55:23 -0500

    Fully impemented ConvertHandler.getNum(), passing related unit tests.

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,51 +2,68 @@
 *
 *
 *       Complete the handler logic below
-*       
-*       
+*
+*
 */
 
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
-    var result;
-    
+    let result;
+    let letterRegex = /[a-z]/i;
+    let fractionRegex = /[\/]/g;
+    let unitIndex = letterRegex.exec(input).index;
+
+    if (unitIndex == 0) {
+      result = 1;
+    } else {
+      result = input.substring(0, unitIndex).replace(/\s/g, '');
+      if (fractionRegex.test(result) == true) {
+        if (result.match(fractionRegex).length !== 1) {
+          result = 'Invalid Number';
+        } else {
+          result = result.split('/').reduce((a,b) => a/(+b||0))
+        }
+      } else {
+        result = Number(result);
+      }
+    }
     return result;
   };
-  
+
   this.getUnit = function(input) {
     var result;
-    
+
     return result;
   };
-  
+
   this.getReturnUnit = function(initUnit) {
     var result;
-    
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     var result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
-    
+
     return result;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     var result;
-    
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
