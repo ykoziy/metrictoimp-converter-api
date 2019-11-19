dd35df41aa93afae4641670a1a8c0c513a36d63d
Yuriy Koziy <yukoziy@gmail.com>
2019-11-19 16:10:32 -0500

    Added helmet middleware for basic security

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
-
+const helmet    = require('helmet');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+app.use(helmet());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
